a5bd95991bc06749fbb5d9bd3dd6375f4df2a922
hrfreiitas <hrfreiitas@gmail.com>
2023-09-27 15:32:25 +0100

    Creating a BloggerName
    Blogger name

--- src/minihomework1/Minihomework1.java
+++ src/minihomework1/Minihomework1.java
@@ -17,19 +17,44 @@ public class Minihomework1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner myKB = new Scanner(System.in);
+        
+        System.out.println("LET`S CREATE A USERNAME FOR YOU !! ");
+        
+        Scanner myKB = new Scanner(System.in);
     String name = "";//user name
+    String secondName;
     int age;
     
-        System.out.println("Please enter your First name.");//for the user tapy theirs names
+        System.out.println("Please enter your First and Second name.");//for the user tapy theirs names
     
         name = myKB.nextLine();
     
         System.out.println("Please enter your age.");
     
         age = myKB.nextInt();
+        
+         int space = name.indexOf(" "); // to located the "space" betwen then
      
+        try {
+
+            String myPiece = name.substring(0, 1); // this is for the firts character to be Capital
+            myPiece = myPiece.toUpperCase(); //first character to be Capital
+            secondName = name.substring(space + 1,space +2); // to take the first character of second name 
+            secondName = secondName.toUpperCase();//second character to be Capital
+
+            if (name.matches("[A-Za-z ]+")) {  // if it is a letter show up for the user next line 
+                System.out.println("The Mr " + myPiece + secondName + " blogger." + age);
+            } else { // if it is not a letter show up the next line 
+
+                System.out.println("Dont type a number");
+            }
+
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+
+    }
         
     }
     
-}
+
