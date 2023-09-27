942c202216041e5b7bd6981bfde97393c5187c70
hrfreiitas <hrfreiitas@gmail.com>
2023-09-27 15:20:00 +0100

    Initial commit

---
+++ src/minihomework1/Minihomework1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomework1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hrfre
+ */
+public class Minihomework1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner myKB = new Scanner(System.in);
+    String name = "";//user name
+    System.out.println("Please enter your First name");//for the user tapy theirs names
+    
+    name = myKB.nextLine();
+    
+        
+    }
+    
+}
